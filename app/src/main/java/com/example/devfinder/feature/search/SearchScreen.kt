737eeeb209948ca666228ce103078dc06cc183ad
Murilo <127051508+murilo587@users.noreply.github.com>
2026-05-08 12:35:25 -0300

    feat: add navigation to profile screen on user click

--- app/src/main/java/com/example/devfinder/feature/search/SearchScreen.kt
+++ app/src/main/java/com/example/devfinder/feature/search/SearchScreen.kt
@@ -1,5 +1,6 @@
 package com.example.devfinder.feature.search
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -23,7 +24,8 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun SearchScreen(
-    viewModel: SearchViewModel
+    viewModel: SearchViewModel,
+    onUserClick: (String) -> Unit
 ) {
     val query by viewModel.query.collectAsState()
     val uiState by viewModel.uiState.collectAsState()
@@ -57,6 +59,7 @@ fun SearchScreen(
                                 modifier = Modifier
                                     .fillMaxWidth()
                                     .padding(vertical = 4.dp)
+                                    .clickable{ onUserClick(user.login) }
                             ) {
                                 Text(user.login)
                             }
