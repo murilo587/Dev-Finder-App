d081242c04bf1415a3c07d749abc1aef64849379
Murilo <127051508+murilo587@users.noreply.github.com>
2026-05-08 12:36:18 -0300

    feat: implement Jetpack Navigation and routing logic

--- app/src/main/java/com/example/devfinder/MainActivity.kt
+++ app/src/main/java/com/example/devfinder/MainActivity.kt
@@ -5,43 +5,47 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentManager
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.devfinder.feature.profile.ProfileScreen
+import com.example.devfinder.feature.profile.ProfileViewModel
+import com.example.devfinder.feature.search.SearchScreen
+import com.example.devfinder.feature.search.SearchViewModel
 import com.example.devfinder.ui.theme.DevFinderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DevFinderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "search") {
+                        composable("search") {
+                            val viewModel: SearchViewModel = hiltViewModel()
+                            SearchScreen(viewModel = viewModel, onUserClick = { username ->
+                                navController.navigate("profile/$username")})
+                        }
+                        composable("profile/{username}") { backStackEntry ->
+                            val username = backStackEntry.arguments?.getString("username") ?: ""
+                            val viewModel: ProfileViewModel = hiltViewModel()
+                            ProfileScreen(viewModel = viewModel, username = username, onBackClick = { navController.popBackStack() })
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DevFinderTheme {
-        Greeting("Android")
     }
 }
