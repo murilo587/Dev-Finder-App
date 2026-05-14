d9a0455946e9c1e828a4d35bb62c009787c9c1dd
Murilo <127051508+murilo587@users.noreply.github.com>
2026-05-14 00:32:13 -0300

    feat: add user profile image to search card

--- app/src/main/java/com/example/devfinder/feature/search/SearchScreen.kt
+++ app/src/main/java/com/example/devfinder/feature/search/SearchScreen.kt
@@ -3,25 +3,40 @@ package com.example.devfinder.feature.search
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.example.devfinder.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(
     viewModel: SearchViewModel,
@@ -29,46 +44,56 @@ fun SearchScreen(
 ) {
     val query by viewModel.query.collectAsState()
     val uiState by viewModel.uiState.collectAsState()
+    Scaffold(topBar = { TopAppBar(title = { Text("Search") }) }) { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = query,
+                onValueChange = { viewModel.handleIntent(SearchIntent.OnQueryChanged(it))},
+                label = { Text("Pesquisar usuário") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-    Column(
-        modifier = Modifier.fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Spacer(modifier = Modifier.height(76.dp))
-        OutlinedTextField(
-            value = query,
-            onValueChange = { viewModel.handleIntent(SearchIntent.OnQueryChanged(it))},
-            label = { Text("Pesquisar usuário") },
-            modifier = Modifier.fillMaxWidth()
-        )
+            Box(modifier = Modifier.fillMaxSize()) {
+                when (val state = uiState) {
+                    is SearchUiState.Idle -> {
+                        Text("Digite para Buscar...")
+                    }
+                    is SearchUiState.Loading -> {
+                        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    }
+                    is SearchUiState.Success -> {
+                        LazyColumn {
+                            items(state.users.items) { user ->
+                                Card(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 5.dp)
+                                        .clickable{ onUserClick(user.login) }
+                                ) {
+                                    Row(modifier = Modifier.padding(20.dp), verticalAlignment = Alignment.CenterVertically) {
+                                        AsyncImage(model = user.avatarUrl, contentDescription = "UserImage",
+                                            contentScale = ContentScale.Fit,
+                                            placeholder = painterResource(R.drawable.user_icon),
+                                            modifier = Modifier.size(55.dp)
+                                                .clip(CircleShape)
+                                        )
+                                        Spacer(modifier = Modifier.width(16.dp))
+                                        Text(text = user.login, fontSize = 20.sp, fontWeight = FontWeight.Medium)
+                                    }
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Box(modifier = Modifier.fillMaxSize()) {
-            when (val state = uiState) {
-                is SearchUiState.Idle -> {
-                    Text("Digite para Buscar...")
-                }
-                is SearchUiState.Loading -> {
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-                }
-                is SearchUiState.Success -> {
-                    LazyColumn {
-                        items(state.users.items) { user ->
-                            Card(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(vertical = 4.dp)
-                                    .clickable{ onUserClick(user.login) }
-                            ) {
-                                Text(user.login)
+                                }
                             }
                         }
                     }
-                }
-                is SearchUiState.Error -> {
-                    Text(text = state.message,
-                        color = MaterialTheme.colorScheme.error)
+                    is SearchUiState.Error -> {
+                        Text(text = state.message,
+                            color = MaterialTheme.colorScheme.error)
+                    }
                 }
             }
         }
