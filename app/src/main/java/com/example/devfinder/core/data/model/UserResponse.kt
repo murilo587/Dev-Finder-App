2a3f7533bf0d4c4dbdf25895040628aa6e079f24
Murilo <127051508+murilo587@users.noreply.github.com>
2026-05-14 00:30:58 -0300

    feat: add user profile image and bio text

--- app/src/main/java/com/example/devfinder/core/data/model/UserResponse.kt
+++ app/src/main/java/com/example/devfinder/core/data/model/UserResponse.kt
@@ -11,6 +11,8 @@ data class UserResponse(
     val bio: String,
     @SerializedName("public_repos")
     val publicRepos: Int,
+    @SerializedName("avatar_url")
+    val avatarUrl: String,
     val followers: Int,
     val following: Int
 )
@@ -19,7 +21,9 @@ data class SearchUserItem(
     val id: Long,
     val login: String,
     @SerializedName("html_url")
-    val htmlUrl: String
+    val htmlUrl: String,
+    @SerializedName("avatar_url")
+    val avatarUrl: String
 )
 
 data class UserListResponse(
--- app/src/main/java/com/example/devfinder/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/devfinder/feature/profile/ProfileScreen.kt
@@ -1,20 +1,36 @@
 package com.example.devfinder.feature.profile
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.example.devfinder.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     viewModel: ProfileViewModel,
@@ -26,26 +42,63 @@ fun ProfileScreen(
     LaunchedEffect(username) {
         viewModel.handleIntent(ProfileIntent.LoadUser(username))
     }
+    Scaffold() { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(16.dp)
+                .padding(paddingValues)
+        ) {
+            Spacer(modifier = Modifier.height(76.dp))
+            Button(onClick = onBackClick) {
+                Text("Voltar")
+            }
+            when (val state = uiState) {
+                is ProfileUiState.Loading -> {
+                    CircularProgressIndicator()
+                }
+                is ProfileUiState.Success -> {
+                    Column(modifier = Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally) {
+                        AsyncImage(
+                            model = state.user.avatarUrl,
+                            contentDescription = "user image",
+                            placeholder = painterResource(R.drawable.user_icon),
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier.size(230.dp)
+                                .clip(shape = CircleShape)
+                                .border(
+                                    width = 2.dp,
+                                    color = Color.Gray,
+                                    shape = CircleShape
+                                )
+                        )
+                        Spacer(modifier = Modifier.height(26.dp))
+                        Text(text = "@${state.user.login}", fontSize = 26.sp)
+                        val bio = if (state.user.bio.length > 30) {
+                            state.user.bio.take(30) + "..."
+                        } else {
+                            state.user.bio
+                        }
+                        if (!state.user.bio.isNullOrBlank()) {
+                            Text(
+                                text = bio,
+                                fontSize = 16.sp,
+                                fontStyle = FontStyle.Italic,
+                                color = Color.Gray,
+                                maxLines = 2,
+                                overflow = TextOverflow.Ellipsis,
+                                modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
+                            )
+                        }
+                    }
 
-    Column(
-        modifier = Modifier.fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Spacer(modifier = Modifier.height(76.dp))
-        Button(onClick = onBackClick) {
-            Text("Voltar")
+                }
+                is ProfileUiState.Error -> {
+                    Text("Erro: ${state.message}")
+                }
+                is ProfileUiState.Idle -> {}
+            }
         }
-        when (val state = uiState) {
-            is ProfileUiState.Loading -> {
-                CircularProgressIndicator()
-            }
-            is ProfileUiState.Success -> {
-                Text(state.user.login)
-            }
-            is ProfileUiState.Error -> {
-                Text("Erro: ${state.message}")
-            }
-            is ProfileUiState.Idle -> {}
-        }
+
     }
+
 }
