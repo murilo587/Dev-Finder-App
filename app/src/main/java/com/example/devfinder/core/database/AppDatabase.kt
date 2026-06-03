7b1a55a536856eae97258e2ed081b2fc492fae43
Murilo <127051508+murilo587@users.noreply.github.com>
2026-06-03 11:38:19 -0300

    feat: add new entities to AppDatabase

--- app/src/main/java/com/example/devfinder/core/database/AppDatabase.kt
+++ app/src/main/java/com/example/devfinder/core/database/AppDatabase.kt
@@ -4,8 +4,14 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.devfinder.core.database.dao.FavoriteDao
 import com.example.devfinder.core.database.model.FavoriteUserEntity
+import com.example.devfinder.core.database.model.UserRepositoryEntity
+import com.example.devfinder.core.database.model.UserStarredEntity
 
-@Database(entities = [FavoriteUserEntity::class], version = 2, exportSchema = false)
-abstract class AppDatabase: RoomDatabase() {
+@Database(
+    entities = [
+        FavoriteUserEntity::class,
+        UserRepositoryEntity::class, UserStarredEntity::class], version = 3, exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
     abstract fun favoriteDao(): FavoriteDao
 }
