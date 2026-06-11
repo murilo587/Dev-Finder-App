b42233077ce0b27b63ef5959cc2775f0401d7df3
Murilo <127051508+murilo587@users.noreply.github.com>
2026-06-11 11:38:03 -0300

    test: update profile viewmodel unit tests

--- app/src/test/java/com/example/devfinder/feature/ProfileViewModelTest.kt
+++ app/src/test/java/com/example/devfinder/feature/ProfileViewModelTest.kt
@@ -2,6 +2,8 @@ package com.example.devfinder.feature
 
 import androidx.lifecycle.SavedStateHandle
 import app.cash.turbine.test
+import com.example.devfinder.core.data.mapper.toDomain
+import com.example.devfinder.core.data.model.UserResponse
 import com.example.devfinder.core.domain.GithubRepository
 import com.example.devfinder.core.domain.model.User
 import com.example.devfinder.feature.profile.ProfileIntent
@@ -14,9 +16,12 @@ import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.AfterEach
@@ -26,6 +31,7 @@ import org.junit.jupiter.api.Test
 class ProfileViewModelTest {
     private lateinit var viewModel: ProfileViewModel
     private lateinit var mockRepository: GithubRepository
+    private lateinit var savedStateHandle: SavedStateHandle
     private val testDispatcher = StandardTestDispatcher()
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -33,14 +39,15 @@ class ProfileViewModelTest {
     fun setup() {
         Dispatchers.setMain(testDispatcher)
         mockRepository = mockk()
-        val savedStateHandle = SavedStateHandle(
+        savedStateHandle = SavedStateHandle(
             mapOf(
                 "username" to "kotlin",
-                "isSaved" to false
+                "isSaved" to false,
+                "userId" to 1
             )
         )
-        viewModel = ProfileViewModel(mockRepository, savedStateHandle)
     }
+
     @OptIn(ExperimentalCoroutinesApi::class)
     @AfterEach
     fun tearDown() {
@@ -48,46 +55,169 @@ class ProfileViewModelTest {
     }
 
     @Test
-    fun `initial state should be Idle`() {
-        viewModel.uiState.value shouldBe ProfileUiState.Idle
+    fun `should load user from remote when not saved locally`() = runTest {
+        val mockUserResponse = UserResponse(
+            id = 1,
+            login = "kotlin",
+            avatarUrl = "mockAvatarUrl",
+            htmlUrl = "mockHtmlUrl",
+            name = "kotlin",
+            bio = "mockBio",
+            followers = 1,
+            following = 1,
+            publicRepos = 1
+        ).toDomain()
+        coEvery { mockRepository.checkIsFavoriteDirect(1) } returns false
+        coEvery { mockRepository.getFavoriteUserByName("kotlin") } returns null
+        coEvery { mockRepository.getUser("kotlin") } returns Result.success(mockUserResponse)
+        coEvery { mockRepository.isFavorite(1) } returns flowOf(false)
+
+        viewModel = ProfileViewModel(mockRepository,testDispatcher, savedStateHandle)
+
+
+        viewModel.uiState.test {
+            awaitItem() shouldBe ProfileUiState.Idle
+            awaitItem() shouldBe ProfileUiState.Loading
+            awaitItem() shouldBe ProfileUiState.Success(mockUserResponse)
+        }
+
+        coVerify(exactly = 1) {
+            mockRepository.getUser("kotlin")
+        }
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `Success should emit Idle, Loading, Success`() = runTest {
-        val mockUserResponse = User(
+    fun `should load user from database when saved locally`() = runTest {
+        val mockUser = User(
             id = 1,
-            login = "mockUser",
+            login = "kotlin",
             avatarUrl = "mockAvatarUrl",
             htmlUrl = "mockHtmlUrl",
             bio = "mockBio",
         )
-        coEvery { mockRepository.getUser("kotlin")} returns Result.success(mockUserResponse)
-        coEvery { mockRepository.isFavorite(any()) } returns flowOf(false)
+        coEvery { mockRepository.checkIsFavoriteDirect(1) } returns true
+        coEvery { mockRepository.getFavoriteUserByName("kotlin") } returns mockUser
+        coEvery { mockRepository.isFavorite(1) } returns flowOf(true)
+
+        viewModel = ProfileViewModel(mockRepository,testDispatcher, savedStateHandle)
 
         viewModel.uiState.test {
             awaitItem() shouldBe ProfileUiState.Idle
-            viewModel.handleIntent(ProfileIntent.LoadUser("kotlin"))
             awaitItem() shouldBe ProfileUiState.Loading
-            awaitItem().shouldBeInstanceOf<ProfileUiState.Success>()
+            awaitItem() shouldBe ProfileUiState.Success(mockUser)
+        }
 
-            coVerify(exactly = 1) { mockRepository.getUser("kotlin") }
-            cancelAndIgnoreRemainingEvents()
+        coVerify(exactly = 1) {
+            mockRepository.getFavoriteUserByName("kotlin")
         }
     }
 
     @Test
-    fun `Error should emit Idle, Loading, Error`() = runTest {
-        coEvery { mockRepository.getUser("kotlin")} returns Result.failure(Exception("Error"))
+    fun `should emit error when remote fetch fail`() = runTest {
+        val exception = Exception("Unknown Error")
+        coEvery { mockRepository.checkIsFavoriteDirect(1) } returns false
+        coEvery { mockRepository.getFavoriteUserByName("kotlin") } returns null
+        coEvery { mockRepository.getUser("kotlin") } returns Result.failure(exception)
+
+        viewModel = ProfileViewModel(mockRepository,testDispatcher, savedStateHandle)
 
         viewModel.uiState.test {
             awaitItem() shouldBe ProfileUiState.Idle
-            viewModel.handleIntent(ProfileIntent.LoadUser("kotlin"))
             awaitItem() shouldBe ProfileUiState.Loading
-            awaitItem().shouldBeInstanceOf<ProfileUiState.Error>()
+            awaitItem() shouldBe ProfileUiState.Error("Unknown Error")
+        }
 
-            coVerify(exactly = 1) { mockRepository.getUser("kotlin") }
-            cancelAndIgnoreRemainingEvents()
+        coVerify(exactly = 1) {
+            mockRepository.getUser("kotlin")
         }
     }
 
+    @Test
+    fun `should observe if the user is saved`() = runTest {
+        val mockUser = User(
+            id = 1,
+            login = "kotlin",
+            avatarUrl = "mockAvatarUrl",
+            htmlUrl = "mockHtmlUrl",
+            bio = "mockBio",
+        )
+        coEvery { mockRepository.checkIsFavoriteDirect(1) } returns true
+        coEvery { mockRepository.getFavoriteUserByName("kotlin") } returns mockUser
+        coEvery { mockRepository.isFavorite(1) } returns flowOf(true)
+
+        viewModel = ProfileViewModel(mockRepository,testDispatcher, savedStateHandle)
+
+        viewModel.isFavorite.test {
+            awaitItem() shouldBe false
+            awaitItem() shouldBe true
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `toggleFavorite should save favorited user data in local database`() = runTest {
+        val mockUserResponse = UserResponse(
+            id = 1,
+            login = "kotlin",
+            avatarUrl = "mockAvatarUrl",
+            htmlUrl = "mockHtmlUrl",
+            name = "kotlin",
+            bio = "mockBio",
+            followers = 1,
+            following = 1,
+            publicRepos = 1
+        ).toDomain()
+        coEvery { mockRepository.checkIsFavoriteDirect(1) } returns false
+        coEvery { mockRepository.getFavoriteUserByName("kotlin") } returns null
+        coEvery { mockRepository.getUser("kotlin") } returns Result.success(mockUserResponse)
+        coEvery { mockRepository.isFavorite(1) } returns flowOf(false)
+        coEvery { mockRepository.saveFavorite(any()) } returns Unit
+        coEvery { mockRepository.getRepos(any()) } returns Result.success(emptyList())
+        coEvery { mockRepository.getStarredRepos(any()) } returns Result.success(emptyList())
+        coEvery { mockRepository.saveRepositories(any(), any()) } returns Unit
+        coEvery { mockRepository.saveStarredRepositories(any(), any()) } returns Unit
+
+
+        viewModel = ProfileViewModel(mockRepository,testDispatcher, savedStateHandle)
+
+        advanceUntilIdle()
+
+        viewModel.handleIntent(ProfileIntent.ToggleFavorite(mockUserResponse))
+
+        advanceUntilIdle()
+
+        coVerify(exactly = 1) {
+            mockRepository.saveFavorite(mockUserResponse)
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `toggleFavorite should remove favorited user data in local database`() = runTest {
+        val mockUser = User(
+            id = 1,
+            login = "kotlin",
+            avatarUrl = "mockAvatarUrl",
+            htmlUrl = "mockHtmlUrl",
+            bio = "mockBio",
+        )
+        coEvery { mockRepository.checkIsFavoriteDirect(1) } returns true
+        coEvery { mockRepository.getFavoriteUserByName("kotlin") } returns mockUser
+        coEvery { mockRepository.isFavorite(1) } returns flowOf(true)
+        coEvery { mockRepository.removeFavorite(any()) } returns Unit
+        coEvery { mockRepository.getRepos(any()) } returns Result.success(emptyList())
+        coEvery { mockRepository.removeRepositories(any()) } returns Unit
+        coEvery { mockRepository.removeStarredRepositories(any()) } returns Unit
+
+        viewModel = ProfileViewModel(mockRepository,testDispatcher, savedStateHandle)
+        advanceUntilIdle()
+        viewModel.handleIntent(ProfileIntent.ToggleFavorite(mockUser))
+        advanceUntilIdle()
+        coVerify(exactly = 1) {
+            mockRepository.removeFavorite(mockUser)
+            mockRepository.removeRepositories(1)
+            mockRepository.removeStarredRepositories(1)
+        }
+    }
 }
